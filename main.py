9caf344d3783f408cc9e54a8a93391d026d3a52a
Christopher Nge <christopher_nge_from.tp@tech.gov.sg>
2022-08-13 12:56:38 +0800

    Add Lambda Function

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ import bot
 import Library.requests as requests
 import order
 
-
 def run():
     setEnvironmentVar()
-
     # bot.runBot()
-
     # Run Client Instance
     client = API(access_token=os.getenv('AccessToken'))
 
@@ -22,22 +19,21 @@ def run():
     # # Get Account Balance
     # print(portfolio.accountBalance(client))
 
-    # # Create Order
-    # print(order.createOrder(client, 100000, "FxSpot", "Sell", "Market", 22)) # EURUSD
+    # Create Order
+    print(order.createOrder(client, 100000, "FxSpot", "Sell", "Market", 22)) # GBPAUD
 
-    # # View Current Positions
-    positions = order.viewPositions(client)
-    print(positions)
+    # View Current Positions
+    # positions = order.viewPositions(client)
+    # print(positions)
 
 def setEnvironmentVar():
     os.environ['AccountGroupKey'] = "ECZ7QD0zH4SlF33Ulun|KA=="
     os.environ['AccountKey'] = "ECZ7QD0zH4SlF33Ulun|KA=="
     os.environ['ClientKey'] = "ECZ7QD0zH4SlF33Ulun|KA=="
-    os.environ['AccessToken'] = "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoiRUNaN1FEMHpINFNsRjMzVWx1bnxLQT09IiwiY2lkIjoiRUNaN1FEMHpINFNsRjMzVWx1bnxLQT09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiMjJlOTAyYzFhMjFmNGI5M2IyZGE5MDdmMWY5YTY5YjEiLCJkZ2kiOiI4NCIsImV4cCI6IjE2NjAyOTI2MDQiLCJvYWwiOiIxRiJ9.LCNDJuV4qSviEUOcmbhYd5TYTrSEtRtR3sUIGdsatXcrZ5przSaHH3aCTXSjxU9S6MLAMfLqlqwuvh-1AqmOPg"
+    os.environ['AccessToken'] = "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoiRUNaN1FEMHpINFNsRjMzVWx1bnxLQT09IiwiY2lkIjoiRUNaN1FEMHpINFNsRjMzVWx1bnxLQT09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiNjIzY2VhOTFhMDdhNDE0MzhjODBmYjQ1YjljMWJiNmEiLCJkZ2kiOiI4NCIsImV4cCI6IjE2NjAzNzAzMjQiLCJvYWwiOiIxRiJ9.mMzhR8cG3yv-m9EueGpbwrFu9Oa4gaMiqrNcqxNJH674MBS7RO_YxQGrHA16FAiqW38hbCcn4eqm8Nzhf5NCRA"
     os.environ['AccountId'] = "17001666"
     os.environ['ClientId'] = "17001666"
     os.environ['TelegramBot'] = "5456469961:AAHRLzVTU-S4Roe2tKCY3Qd7J7uGm_DHiGo"
     os.environ['ChatID'] = "266003499"
-
 
 run()
